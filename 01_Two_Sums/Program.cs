95833b2846f6d04b87a2c7dd5f631c11e0bddeac
Kim Newton <kimberly.jacques@efrei.net>
2022-12-14 18:54:28 +0100

    Adding Two Sum Problem Solution

---
+++ 01_Two_Sums/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<int, int> numbersToFind = new();
+
+int[]? TwoSum(int[] nums, int target)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (!numbersToFind.ContainsKey(nums[i]))
+        {
+            if (!numbersToFind.ContainsKey(target - nums[i]))
+                numbersToFind.Add(target - nums[i], i);
+            continue;
+        }
+        return new int[] { numbersToFind[nums[i]], i };
+    }
+    return null;
+}
+
