565c7369ac00475206ad97ddbb09c4c93043887c
Kim Newton <kimberly.jacques@efrei.net>
2022-12-24 11:10:47 +0100

    Adding 09_Palindrome_Number

---
+++ 09_Palindrome_Number/Program.cs
@@ -0,0 +1,16 @@
+﻿bool IsPalindrome(int number)
+{
+    int reversedNumber = 0;
+    int reminderNumber = number;
+
+    if (number < 0)
+        return false;
+
+    while (number > 0)
+    {
+        reversedNumber = reversedNumber* 10 + number % 10;
+        number /= 10;
+    }   
+    return reversedNumber == reminderNumber ;
+}
+
